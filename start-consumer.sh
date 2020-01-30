bd559ca6ae5c7b4fb62d5007c43cef152da508f3
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-01-30 21:44:50 +0000

    Added requirements.txt

--- start-consumer.sh
+++ start-consumer.sh
@@ -2,4 +2,4 @@
 
 sleep 20s
 echo "Running Consumer..."
-source /usr/local/fs-python/env/bin/activate && cd /usr/local/fs-python && python3 consumer.py
+source /usr/local/python/env/bin/activate && cd /usr/local/python && python3 /etc/git-repo/consumer.py
